15e33cb90527170dd63e271cdaeb41e9aed116ff
Danielly <daniellycmcosta@gmail.com>
2018-11-03 17:12:44 -0300

    Versão Funcionando

--- FenwickTree.java
+++ FenwickTree.java
@@ -1,153 +1,92 @@
 package lab3;
 
 public class FenwickTree {
-
     int value;
-    int leftSize;
-    FenwickTree left = new FenwickTree();
-    FenwickTree right = new FenwickTree();
-
-    public FenwickTree() {
-
-    }
-
-    FenwickTree(int value) {
-
-        this.value = value;
-    }
-
-    public FenwickTree(int leftSize, FenwickTree left, FenwickTree right) {
-        this.leftSize = leftSize;
-        this.left = left;
-        this.right = right;
-    }
-
-    public FenwickTree(int value, int leftSize, FenwickTree left, FenwickTree right) {
-        this.value = value;
-        this.leftSize = leftSize;
-        this.left = left;
-        this.right = right;
-    }
-	
-	
-    void prefixSum(int upto) {
-
-        if(upto != 0){
-           if (leftsize == 0) {
-                return value;
-            } else {
-                if (i < leftsize) {
-			return left.prefixSum(upto);	 
-		}else {
-			return right.prefixSum(right-leftSize) + left.value;
-		}
-        }
-
-    }
-
+    int leftsize;
+    FenwickTree left;
+    FenwickTree right;
     
-    int between(int lo, int hi){
-        return prefixSum(lo) + prefixSum(hi-leftSize);
-    }
-
+    FenwickTree (int value){
+    	this.value=value;
+    	this.leftsize=0;
+    	this.left=null;
+    	this.right=null; 
+    } 
     
-    int size(FenwickTree no) {
-        if (no == null) {
-            return 0;
+    FenwickTree(int leftsize, FenwickTree left, FenwickTree right){
+    	this.value=left.value +right.value;
+    	this.leftsize=leftsize;
+    	this.left= left;
+    	this.right=right;    
+    }
+    
+    FenwickTree(int value, int leftsize, FenwickTree left, FenwickTree right){
+    	this.value=value;
+    	this.leftsize=leftsize;
+    	this.left= left;
+    	this.right=right;    
+    }
+    
+    public String toString(){
+        if(this.left==null && this.right==null){
+            return "["+ this.value + "," + this.leftsize+"]";
         }
-        if (no.left == null && no.right == null) {
+        else if(this.left==null){
+            return "["+ this.value+","+ this.leftsize+","+ this.right.toString()+"]";
+        }
+        else if(this.right==null){
+            return "["+ this.value +","+ this.leftsize +","+ this.left.toString()+"]";
+            
+        }
+        else{
+            return "["+ this.value+","+ this.leftsize+","+ this.right.toString()+","+ this.left.toString()+"]";
+  
+        }
+    }
+    
+    static FenwickTree allZeros(int n){
+        if (n==0) return null;
+        if (n==1) return new FenwickTree(0);
+        int m = n/2;
+        return new FenwickTree(0, n-m, allZeros(n-m), allZeros(m));
+    }
+    
+    public int size(){
+        if(this.left==null && this.right==null){
             return 1;
         }
-	if((!inicio.left) && (!inicio.right)){
-		return 1 + size(inicio.right) + size(inicio.left);
-	}else{
-		return 0 + size(inicio.right) + size(inicio.left);
-	}
- }
-
-    void increment(int i, int delta) {
-        if (left!=null && right!=null) {
-            value= value+delta;
-        }
-      
-            if (i < leftSize) {
-		value = value+delta;    
-                left.increment(i, delta);
-            } else {
-		value = value+delta;    
-                right.increment(i-leftSize, delta);
-            }
-        
+        return this.leftsize+ this.right.size(); 
     }
-
-    public String toString() {
-        //acho que ta errado
-        //return "Valor "+ this.value + "/n" + this.leftSize + "/n" + left.toString() + right.toString();
-        FenwickTree novo = new FendiwckTree();
-        //if (novo != null) {
-        //    novo.left.toString();
-        //    System.out.print(novo.value + " ");
-        //    novo.right.toString();
-      //  }
-	 if (novo!=null){
-		Sytem.out.printl(novo.value);
-		toString(novo.left);
-		toString(novo.right);
+ 
+    public void increment(int i, int delta){
+        this.value += delta;
+        if(this.left==null && this.right==null) return ;
+        else if (this.leftsize<i){
+            this.left.increment(i,delta) ;
+        }
+        else{
+            this.right.increment(i, delta);
         }
     }
-
-    static FenwickTree allZeros(int n) {
-        if (n == 0) {
-            return null;
-        }
-        if (n == 1) {
-            return new FenwickTree(0);
-        }
-        int m = n / 2;
-        return new FenwickTree(0, n - m, allZeros(n - m), allZeros(m));
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public void setValue(int value) {
-        this.value = value;
-    }
-
-    public int getLeftSize() {
-        return leftSize;
-    }
-
-    public void setLeftSize(int leftSize) {
-        this.leftSize = leftSize;
-    }
-
-    public FenwickTree getLeft() {
-        return left;
-    }
-
-    public void setLeft(FenwickTree left) {
-        this.left = left;
-    }
-
-    public FenwickTree getRight() {
-        return right;
-    }
-
-    public void setRight(FenwickTree right) {
-        this.right = right;
-    }
-
-  
-        public static void main(String[] args) {
-            System.out.println("Construcao de FenwickTree(3) : " + new FenwickTree(3));
-            System.out.println("Construcao da arvore da figura : "
-                    + new FenwickTree(3, new FenwickTree(1, new FenwickTree(4),
-                            new FenwickTree(1, new FenwickTree(2), new FenwickTree(5))),
-                            new FenwickTree(1, new FenwickTree(3),
-                                    new FenwickTree(1, new FenwickTree(6), new FenwickTree(1)))));
-        }
     
-
+    public int prefixSum(int upto){
+        if(this.left==null)
+        {
+            return 0;
+        }
+        else if (this.leftsize< upto)
+        {
+          return this.value - this.right.value + this.right.prefixSum(upto-this.leftsize);  
+        }
+        else if (upto==this.leftsize)
+        {
+           return this.value; 
+        }    
+        else if (this.leftsize> upto)
+        {
+           return this.left.prefixSum(upto);  
+        }
+        else
+        return 1;
+    }
 }
